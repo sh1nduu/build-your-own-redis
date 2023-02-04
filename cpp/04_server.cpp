696b11a541b822272a2baff74952eb43ea063f57
sh1nduu <sh1nduu@users.noreply.github.com>
2023-02-05 06:08:16 +0900

    (chapter 4) parse simple messaging protocol

--- cpp/04_server.cpp
+++ cpp/04_server.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
 
 static void msg(const char *msg) {
   printf("%s\n", msg);
@@ -18,16 +20,75 @@ static void die(const char *msg) {
     abort();
 }
 
-static void do_something(int connfd) {
-  char rbuf[64] = {};
-  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-  if (n < 0) {
-    msg("read() error");
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
+    }
+    assert((size_t) rv <= n);
+    n -= (size_t) rv;
+    buf += rv;
   }
-  printf("client says: %s\n", rbuf);
-  char wbuf[] = "world";
-  write(connfd, wbuf, strlen(wbuf));
+  return 0;
 }
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1;
+    }
+    assert((size_t) rv <= n);
+    n -= (size_t) rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd) {
+  // 4 bytes header
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  int32_t err = read_full(connfd, rbuf, 4);
+  if (err) {
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read() error");
+    }
+    return err;
+  }
+
+  uint32_t len = 0;
+  memcpy(&len, rbuf, 4); // assume little endian
+  if (len > k_max_msg) {
+    msg("too long");
+    return -1;
+  }
+
+  // request body
+  err = read_full(connfd, &rbuf[4], len);
+  if (err) {
+    msg("read() error");
+    return err;
+  }
+
+  // do something
+  rbuf[4 + len] = '\0';
+  printf("client says: %s\n", &rbuf[4]);
+
+  // reply using the same protocol
+  const char reply[] = "world";
+  char wbuf[4 + sizeof(reply)];
+  len = (uint32_t) strlen(reply);
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], reply, len);
+  return write_all(connfd, wbuf, 4 + len);
+}
+
 
 int main() {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -63,7 +124,13 @@ int main() {
 
     }
 
-    do_something(connfd);
+    while (true) {
+      int32_t err = one_request(connfd);
+      if (err) {
+        break;
+      }
+    }
+
     close(connfd);
   }
 
