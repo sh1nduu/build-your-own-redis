1a6683d3e7b78f5b34649b1f3fe26a1e42489112
sh1nduu <sh1nduu@users.noreply.github.com>
2023-02-05 06:09:03 +0900

    (chore) add build script

---
+++ cpp/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+BUILD_DIR=output
+SRC=$1
+
+if [[ $SRC =~ ^[0-9]+_(.+).cpp$ ]]
+then
+  BIN=${BASH_REMATCH[1]}
+  g++ -Wall -Wextra -O2 -g $1 -o $BUILD_DIR/$BIN
+  echo DONE
+else
+  echo failed $SRC
+fi
+
