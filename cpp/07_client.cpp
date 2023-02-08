b117470d9c3e8eaf3fea00eae01c86e3e82ed2b2
sh1nduu <sh1nduu@users.noreply.github.com>
2023-02-09 03:02:03 +0900

    (chore) format code

--- cpp/07_client.cpp
+++ cpp/07_client.cpp
@@ -10,39 +10,48 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static void msg(const char *msg) { printf("%s\n", msg); }
-
-static void die(const char *msg) {
-  int err = errno;
-  fprintf(stderr, "[%d] %s\n", err, msg);
-  abort();
-}
-
 const uint32_t k_max_msg = 4096;
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-  while (n > 0) {
-    ssize_t rv = read(fd, buf, n);
-    if (rv <= 0) {
-      return -1; // error, or unexpected EOF
-    }
-    assert((size_t)rv <= n);
-    n -= (size_t)rv;
-    buf += rv;
-  }
-  return 0;
-}
+static int32_t send_req(int fd, const char *text);
+static int32_t read_res(int fd);
+static int32_t read_full(int fd, char *buf, size_t n);
+static int32_t write_all(int fd, const char *buf, size_t n);
 
-static int32_t write_all(int fd, const char *buf, size_t n) {
-  while (n > 0) {
-    ssize_t rv = write(fd, buf, n);
-    if (rv <= 0) {
-      return -1; // error
-    }
-    assert((size_t)rv <= n);
-    n -= (size_t)rv;
-    buf += rv;
+static void msg(const char *msg);
+static void die(const char *msg);
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    die("socket()");
   }
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
+  int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+  if (rv) {
+    die("connect");
+  }
+
+  // multiple pipelined requests
+  const char *query_list[3] = {"hello1", "hello2", "hello3"};
+  for (size_t i = 0; i < 3; ++i) {
+    int32_t err = send_req(fd, query_list[i]);
+    if (err) {
+      goto L_DONE;
+    }
+  }
+  for (size_t i = 0; i < 3; ++i) {
+    int32_t err = read_res(fd);
+    if (err) {
+      goto L_DONE;
+    }
+  }
+
+L_DONE:
+  close(fd);
   return 0;
 }
 
@@ -95,37 +104,37 @@ static int32_t read_res(int fd) {
   return 0;
 }
 
-int main() {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0) {
-    die("socket()");
-  }
-
-  struct sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_port = ntohs(1234);
-  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
-  int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-  if (rv) {
-    die("connect");
-  }
-
-  // multiple pipelined requests
-  const char *query_list[3] = {"hello1", "hello2", "hello3"};
-  for (size_t i = 0; i < 3; ++i) {
-    int32_t err = send_req(fd, query_list[i]);
-    if (err) {
-      goto L_DONE;
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
     }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
   }
-  for (size_t i = 0; i < 3; ++i) {
-    int32_t err = read_res(fd);
-    if (err) {
-      goto L_DONE;
-    }
-  }
-
-L_DONE:
-  close(fd);
   return 0;
 }
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static void msg(const char *msg) { printf("%s\n", msg); }
+
+static void die(const char *msg) {
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
+
