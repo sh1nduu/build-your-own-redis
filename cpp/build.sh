f688301ffb54a29d28ec4b2d54271e040e8b3126
sh1nduu <sh1nduu@users.noreply.github.com>
2023-02-15 04:16:51 +0900

    (chore) add makefile

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script is obsolete. Use make command.
+
 set -eu -o pipefail
 
 BUILD_DIR=output
@@ -8,7 +10,7 @@ SRC=$1
 if [[ $SRC =~ ^[0-9]+_(.+).cpp$ ]]
 then
   BIN=${BASH_REMATCH[1]}
-  g++ -Wall -Wextra -O2 -std=c++11 -g $1 -o $BUILD_DIR/$BIN
+  g++ -Wall -Wextra -O2 -std=gnu++11 -g $1 -o $BUILD_DIR/$BIN
   echo DONE
 else
   echo failed $SRC
